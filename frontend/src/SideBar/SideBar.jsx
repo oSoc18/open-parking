f787b460bb4d60d268139969528bd9b4ba31d16b
Alex <alexandervandoren@yahoo.com>
2018-07-09 14:14:56 +0200

    Sidebar init

---
+++ frontend/src/SideBar/SideBar.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './SideBar.css';
+
+class SideBar extends Component {
+
+  renderedSideBar(){
+
+  }
+  render() {
+    return (
+      // side bar
+      // content
+      <div className="sideBar">
+        <h1 className="title">Open Parking</h1>
+      </div>
+    );
+  }
+}
+
+export default SideBar;
