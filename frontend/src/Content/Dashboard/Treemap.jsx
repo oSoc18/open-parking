2ea85b614cb609a4d722515d526fb27444f731a2
Unknown <alexandervandoren@yahoo.com>
2018-07-14 18:26:17 +0200

    Fixed bug treemap

--- frontend/src/Content/Dashboard/Treemap.jsx
+++ frontend/src/Content/Dashboard/Treemap.jsx
@@ -10,26 +10,26 @@ var data = 	{
         children: 
         [
             { name: "good", value: 11},
-            { name: "average, value: 88"},
+            { name: "average", value: 88},
             { name: "bad", value: 445}
         ]
     },
         { name: "Zuidwest-Nederland",   children: 
         [
             { name: "good", value: 11},
-            { name: "average, value: 88"},
+            { name: "average", value: 88},
             { name: "bad", value: 111}
         ] } ,
         { name: "Zuid-Nederland",   children: 
         [
             { name: "good", value: 111},
-            { name: "average, value: 88"},
+            { name: "average", value: 88},
             { name: "bad", value: 445}
         ] },
         { name: "Noord-Nederland",   children: 
         [
             { name: "good", value: 311},
-            { name: "average, value: 88"},
+            { name: "average", value: 88},
             { name: "bad", value: 123}
         ] }
     ]
@@ -50,9 +50,11 @@ class Treemap extends Component {
 
   createTreemap(){
     var treemap = d3.treemap()
+    treemap.tile(d3.treemapSquarify)
 
-    treemap.size([400, 200])
-  .paddingOuter(10);
+    treemap.size([960, 570])
+    .paddingTop(20)
+    .paddingInner(2);
 
     this.root.sum(function(d){
         return d.value;
@@ -60,22 +62,34 @@ class Treemap extends Component {
 
     treemap(this.root);
 
-    d3.select('svg')
-  .selectAll('rect')
-  .data(this.root.descendants())
-  .enter()  
+   let nodes = d3.select('svg g')
+   .selectAll('g')
+   .data(this.root.descendants())
+   .enter()
+   .append('g')
+   .attr('transform', function(d) {return 'translate(' + [d.x0, d.y0] + ')'})
+
+   nodes
   .append('rect')
-  .attr('x', function(d) { return d.x0; })
-  .attr('y', function(d) { return d.y0; })
   .attr('width', function(d) { return d.x1 - d.x0; })
   .attr('height', function(d) { return d.y1 - d.y0; })
+
+  nodes
+  .append('text')
+  .attr('dx', 4)
+  .attr('dy', 14)
+  .text(function(d) {
+    return d.data.name;
+  })
   }
+
+  
 
   render() {
 
     return (
       <svg className="TreemapData" >
-           
+           <g></g>
       </svg> 
     );
   } 
