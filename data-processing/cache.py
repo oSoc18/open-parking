bb01945d7409d12bc213d92515b064ff6060ce05
Théo Verhelst <tverhels@ulb.ac.be>
2018-07-17 09:34:59 +0200

    Add docs to cache.py

--- data-processing/cache.py
+++ data-processing/cache.py
@@ -1,3 +1,13 @@
+"""
+Sends a request to every static data link in the index file and stores the
+result in a folder, in a file named after the UUID of the facility. It allows
+to cache locally the static data, in order not to send requests every time.
+It uses a small JSON file to keep track of which files have been cached so far.
+
+Example:
+    python cache.py cache-dir/ cache_list.json data_index_1_3_fixed.json
+
+"""
 import json
 import requests
 from sys import argv
@@ -14,20 +24,25 @@ index_facilities = index_json["ParkingFacilities"]
 
 for facility in index_facilities:
     try:
+        # If we did not downloaded the data yet
         if facility["uuid"] not in cache_list["cached"]:
             print(len(cache_list["cached"]), "/", len(index_facilities))
             print("Requesting ", facility["staticDataUrl"])
+            # Send the request
             result = requests.get(facility["staticDataUrl"])
 
             static_data = None
             if result.status_code == requests.codes.ok and "parkingFacilityInformation" in result.json():
                 static_data = result.json()["parkingFacilityInformation"]
 
+            # Write the result to the output file
             facility["staticData"] = static_data
             with open(join(cache_dir, facility["uuid"] + ".json"), "w") as file:
                 json.dump(facility, file, indent=2)
+            # Update the cache list
             cache_list["cached"].append(facility["uuid"])
             with open(cache_list_filename, "w") as file:
                 json.dump(cache_list, file)
+    # Stop on Ctrl+C (or Ctrl+Break) interrupt
     except KeyboardInterrupt:
         break
