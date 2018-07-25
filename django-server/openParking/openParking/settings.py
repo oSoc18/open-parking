488c26fc116872e931678d10d093777cf020647e
Théo Verhelst <tverhels@ulb.ac.be>
2018-07-25 13:45:25 +0200

    Changes settings for production

--- django-server/openParking/openParking/settings.py
+++ django-server/openParking/openParking/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$jxkedflb1s!p^41xz+5g&$u(o4^gy^!)wwq^k5*v7x3q&j$2k'
+with open(os.path.join(BASE_DIR, "secret_key.txt")) as secret_key_file:
+    SECRET_KEY = secret_key_file.readline().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
@@ -125,3 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
